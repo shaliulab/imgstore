d3defa01bd33a01b084a7c179f9f50c092d78923
antortjim <ntoniohu@gmail.com>
2021-12-23 08:54:01 +0000

    Fix split

--- imgstore/split.py
+++ imgstore/split.py
@@ -10,15 +10,15 @@ class ImgStoreSplitter:
     def __init__(self, store_path):
         self._store = new_for_filename(store_path)
 
-    def split(self, time_range, dest, **kwargs):
+    def split(self, time_range, dest, **user_kwargs):
 
-
-        import ipdb; ipdb.set_trace()
-        kwargs.update({
+        kwargs = {
             "imgshape": self._store._metadata["imgshape"],
             "chunksize": self._store._metadata["chunksize"],
             "framerate": self._store._metadata["framerate"]
-        })
+        }
+
+        kwargs.update(user_kwargs)
 
         dest_store = new_for_format(fmt="mjpeg/avi", path=dest, **kwargs)
 
@@ -28,7 +28,7 @@ class ImgStoreSplitter:
 
         dest_store.add_image(img, 0, 0)
         while timestamp < time_range[-1]:
-            img, (frame_number, timestamp) = self._store._get_next_image()
+            img, (frame_number, timestamp) = self._store.get_next_image()
             frame_number_dest = frame_number - frame_number_0
             timestamp_dest = timestamp - timestamp_0
             dest_store.add_image(img, frame_number_dest, timestamp_dest)
