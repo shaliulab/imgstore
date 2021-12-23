6c99ca71de949b05080aa70067e7d85422cf791d
antortjim <ntoniohu@gmail.com>
2021-12-23 17:09:16 +0000

    Handle passing a store to resizer

--- imgstore/resize.py
+++ imgstore/resize.py
@@ -3,12 +3,17 @@ import json
 import cv2
 import numpy as np
 
-from .stores import new_for_filename, new_for_format
+from .stores import new_for_filename, new_for_format, _ImgStore
 
 class ImgStoreResizer:
 
-    def __init__(self, store_path):
-        self._store = new_for_filename(store_path)
+    def __init__(self, store):
+        if isinstance(store, str):
+            self._store = new_for_filename(store_path)
+        elif isinstance(store, _ImgStore):
+            self._store = store
+
+        self._dest = None
 
     def resize(self, time_range, dest, **user_kwargs):
 
@@ -20,18 +25,18 @@ class ImgStoreResizer:
 
         kwargs.update(user_kwargs)
 
-        dest_store = new_for_format(fmt="mjpeg/avi", path=dest, **kwargs)
+        self._dest = new_for_format(fmt="mjpeg/avi", path=dest, **kwargs)
 
         img, (frame_number_0, timestamp_0) = self._store._get_image_by_time(time_range[0])
 
         timestamp = timestamp_0
 
-        dest_store.add_image(img, 0, 0)
+        self._dest.add_image(img, 0, 0)
         while timestamp < time_range[-1]:
             img, (frame_number, timestamp) = self._store.get_next_image()
             frame_number_dest = frame_number - frame_number_0
             timestamp_dest = timestamp - timestamp_0
-            dest_store.add_image(img, frame_number_dest, timestamp_dest)
+            self._dest.add_image(img, frame_number_dest, timestamp_dest)
 
-        dest_store.close()
+        self._dest.close()
 
