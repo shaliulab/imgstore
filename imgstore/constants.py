66b0e117db4d430955115ec33a2146800d5524a2
antortjim <ntoniohu@gmail.com>
2022-03-28 15:55:04 +0000

    Update imgstore

--- imgstore/constants.py
+++ imgstore/constants.py
@@ -1,3 +1,5 @@
+from cv2 import VideoWriter_fourcc as FourCC
+
 try:
     # python 3
     # noinspection PyProtectedMember
@@ -23,3 +25,17 @@ EXTRA_DATA_FILE_EXTENSIONS = (
 )
 
 FRAME_MD = ("frame_number", "frame_time", "frame_in_chunk")
+ENCODER_FORMAT_GPU="h264_nvenc/mp4" # CUDA
+ENCODER_FORMAT_CPU="mjpeg/avi" # NO CUDA
+
+FMT_TO_CODEC = {
+        "mjpeg": FourCC("M", "J", "P", "G"),
+        "h264/avi": "libx264",
+        "mjpeg/avi": FourCC("M", "J", "P", "G"),
+        "mjpeg/mp4": FourCC("M", "J", "P", "G"),
+        "h264/mkv": FourCC("H", "2", "6", "4"),
+        "avc1/mp4": FourCC("a", "v", "c", "1"),
+        "h264/mp4": FourCC("H", "2", "6", "4"),
+        "x264/mp4": FourCC("X", "2", "6", "4"),
+        "h264_nvenc/mp4": "h264_nvenc",
+    }
