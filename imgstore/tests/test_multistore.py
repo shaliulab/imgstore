694a1b363210a288f6b79eb1a675c0b1dd795694
antortjim <ntoniohu@gmail.com>
2022-06-18 22:40:46 +0000

    Add testing for multi module

--- imgstore/tests/test_multistore.py
+++ imgstore/tests/test_multistore.py
@@ -1,24 +1,27 @@
 import os.path
-from imgstore.stores import multi as multistores
+import imgstore.stores.multi as imgstore
 from imgstore.tests import TEST_DATA_DIR
+from imgstore.constants import STORE_MD_FILENAME
 
-MULTISTORE_PATH = os.path.join(TEST_DATA_DIR, "multistore")
+MULTISTORE_PATH = os.path.join(TEST_DATA_DIR, "multistore", STORE_MD_FILENAME)
 
 
 def test_get_image(store):
-    img, (frame_number, frame_time) = store.get_image(1000)
+    # TODO
+    img, (frame_number, frame_time) = store.get_image(100)
 
 def test_get_nearest_image(store):
-
-    img, (frame_number, frame_time) = store.get_nearest_image(450)
+    # TODO
+    img, (frame_number, frame_time) = store.get_nearest_image(45)
 
 def test_get_next_image(store):
+    # TODO
     img, (frame_number, frame_time) = store.get_next_image()
     
 
 def test_multistore():
 
-    with multistores.new_for_filename(MULTISTORE_PATH) as multistore:
+    with imgstore.new_for_filename(MULTISTORE_PATH) as multistore:
         test_get_image(multistore)
         test_get_nearest_image(multistore)
         test_get_next_image(multistore)
